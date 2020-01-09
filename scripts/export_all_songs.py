f9250093f575885c37256b3ed4aef755037a377b
schef <zvanstefan@gmail.com>
2020-01-09 16:29:01 +0100

    added new generated pdfs

--- scripts/export_all_songs.py
+++ scripts/export_all_songs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import glob
+import subprocess
 
 def run_bash_cmd(cmd):
     output = None
@@ -14,6 +15,44 @@ def add_dir_pdf(repo):
     cmd = "mkdir -p " + repo + "/pdf"
     if (run_bash_cmd(cmd) == None):
         print("add_dir_pdf failed")
+        exit(1)
+
+def rm_pdf(repo):
+    cmd = "rm -rvf " + repo + "/pdf/*"
+    if (run_bash_cmd(cmd) == None):
+        print("rm_pdf failed")
+        exit(1)
+
+def generate_pdf(repo):
+    cmd = "musescore " + repo + "/musescore/* -o " + repo + "/pdf/" + repo.split('/')[1] + ".pdf"
+    if (run_bash_cmd(cmd) == None):
+        print("generate_pdf failed")
+        exit(1)
+
+def add_changes(repo):
+    cmd = "git -C " + repo + " add ."
+    if (run_bash_cmd(cmd) == None):
+        print("add_changes failed")
+        exit(1)
+
+def commit_changes(repo):
+    cmd = "git -C " + repo + " commit -m \"auto generated pdf\""
+    if (run_bash_cmd(cmd) == None):
+        print("commit_changes failed")
+        return False
+    return True
+
+def push_changes(repo):
+    cmd = "git -C " + repo + " push"
+    if (run_bash_cmd(cmd) == None):
+        print("push_changes failed")
+        exit(1)
 
 for repo in glob.iglob('songs/*'):
-    add_dir_pdf(repo)
+    print(repo)
+    # add_dir_pdf(repo)
+    # rm_pdf(repo)
+    # generate_pdf(repo)
+    add_changes(repo)
+    if (commit_changes(repo)):
+        push_changes(repo)
