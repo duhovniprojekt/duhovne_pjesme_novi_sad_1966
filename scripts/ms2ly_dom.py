094dba8b824944918681cbf3742a452d5ffc292c
schef <zvanstefan@gmail.com>
2020-02-10 16:40:44 +0100

    testing

--- scripts/ms2ly_dom.py
+++ scripts/ms2ly_dom.py
@@ -3,62 +3,103 @@
 from xml.dom import minidom
 import sys
 
-indent = ['']
 
+class XmlParser():
+    indent = ['']
 
-def enterNode(node):
-    indent.append(getName(node))
-    attributes = "" if len(getAttributes(
-        node)) == 0 else ' attr="' + str(getAttributes(node)) + '"'
-    text = ' text="' + getText(node) + '"' if len(getText(node)) else ""
-    print("->", getPath() + text + attributes)
+    def __init__(self, node):
+        print("init")
+        self.parseNode(node)
 
+    def enterNode(self, node):
+        self.indent.append(self.getName(node))
+        attributes = "" if len(self.getAttributes(
+            node)) == 0 else ' attr="' + str(self.getAttributes(node)) + '"'
+        text = ' text="' + self.getText(node) + \
+                                        '"' if len(self.getText(node)) else ""
+        print("->", self.getPath() + text + attributes)
 
-def exitNode(node):
-    print("<-", getPath())
-    indent.pop()
+    def exitNode(self, node):
+        print("<-", self.getPath())
+        self.indent.pop()
 
+    def getPath(self, customIndent=None):
+        if (customIndent == None):
+            customIndent = self.indent
+        return('/'.join(customIndent))
 
-def getPath():
-    return('/'.join(indent))
+    def getName(self, node):
+        return node.tagName
 
+    def getText(self, node):
+        rc = []
+        for node in node.childNodes:
+            if node.nodeType == node.TEXT_NODE:
+                rc.append(node.data)
+        return ''.join(rc).strip()
 
-def getName(node):
-    return node.tagName
+    def getAttributes(self, node):
+        attrs = []
+        for attr in node.attributes.items():
+            attrs.append(attr)
+        return attrs
 
+    def getParentNodeByPath(self, currentNode, path):
+        indentPath = list(self.indent)
+        nodeParent = currentNode
+        while (len(indentPath)):
+            indentPath.pop()
+            nodeParent = nodeParent.parentNode
+            if (self.getPath(indentPath) == path):
+                break
+        return nodeParent
 
-def getText(node):
-    rc = []
-    for node in node.childNodes:
-        if node.nodeType == node.TEXT_NODE:
-            rc.append(node.data)
-    return ''.join(rc).strip()
+    def parseElement(self, node):
+        return False
 
+    def parseNode(self, node):
+        if node.nodeType == node.ELEMENT_NODE:
+            self.enterNode(node)
+            if (not self.parseElement(node)):
+                for subnode in node.childNodes:
+                    self.parseNode(subnode)
+            self.exitNode(node)
 
-def getAttributes(node):
-    attrs = []
-    for attr in node.attributes.items():
-        attrs.append(attr)
-    return attrs
-
-
-def parseElement(node):
-    if (getPath() == "/museScore/Score/Staff"):
-        return True
-    return False
-
-
-def parseNode(node):
-    if node.nodeType == node.ELEMENT_NODE:
-        enterNode(node)
+    def isTextInChildren(self, node, text):
+        childrenText = []
         for subnode in node.childNodes:
-            if (not parseElement(node)):
-                parseNode(subnode)
-        exitNode(node)
+            childrenText.append(self.getText(subnode))
+        return text in childrenText
+
+
+class MuseScoreParser(XmlParser):
+    xml_paths = {
+        "chord": "/museScore/Score/Staff/Measure/voice/Chord",
+        "staff": "/museScore/Score/Staff",
+        "title": "/museScore/Score/Staff/VBox/Text"
+    }
+
+    def getStaffId(self, node):
+        staff = self.getParentNodeByPath(node, self.xml_paths['staff'])
+        for attr in self.getAttributes(staff):
+            if (attr[0] == 'id'):
+                return attr[1]
+        return None
+
+    def getTitle(self, node):
+        return "title"
+
+    def parseElement(self, node):
+        if (self.getPath() == self.xml_paths['title']):
+            if (self.isTextInChildren(node, "Title")):
+                print("title", self.getTitle(node))
+        if (self.getPath() == self.xml_paths['chord']):
+            print(self.getStaffId(node))
+            return True
+        return False
 
 
 if __name__ == "__main__":
-    doc = minidom.parse(sys.stdin)
-    rootNode = doc.childNodes[0]
-    # parserAllElements(doc.childNodes)
-    parseNode(rootNode)
+    doc=minidom.parse(sys.stdin)
+    rootNode=doc.childNodes[0]
+    MuseScoreParser(rootNode)
