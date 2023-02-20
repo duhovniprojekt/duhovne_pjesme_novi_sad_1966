d9ccc40ffb4d007f6c1923480554ab14bca8a103
schef <zvanstefan@gmail.com>
2023-02-20 09:50:04 +0100

    working on tests

--- scripts/parser.py
+++ scripts/parser.py
@@ -4,6 +4,282 @@ from xsdata.formats.dataclass.parsers import XmlParser
 
 from msmodel.muse_score import *
 
+@dataclass
+class TypeHeader:
+    title: str
+    subtitle: str
+    composer: str
+    lyricist: str
+
+
+@dataclass
+class TypeKeySignature():
+    number: int
+    key: Optional[str] = None
+
+    parser = {
+        -7 : 'ces',
+        -6 : 'ges',
+        -5 : 'des',
+        -4 : 'as',
+        -3 : 'es',
+        -2 : 'b',
+        -1 : 'f',
+        0 : 'c',
+        1 : 'g',
+        2 : 'd',
+        3 : 'a',
+        4 : 'e',
+        5 : 'h',
+        6 : 'fis',
+        7 : 'cis',
+    }
+
+    def __init__(self, number):
+        self.number = number
+        self.key = self.parser[self.number]
+
+
+@dataclass
+class TypeTimeSignature():
+    n: int
+    d: int
+
+    def getTime(self):
+        return (self.n, self.d)
+        
+
+class TypeDuration():
+    parserDuration = {
+        'whole' : '1',
+        'half' : '2',
+        'quarter' : '4',
+        'eighth' : '8',
+        '16th' : '16',
+        '32nd' : '32'
+    }
+
+
+class TypeRest(TypeDuration):
+    def __init__(self, duration, lastTypeTimeSignature, dots):
+        if (duration == "measure"):
+            if (lastTypeTimeSignature):
+                d, n = lastTypeTimeSignature.getTime()
+                self.rest = 'r%s*%s' % (n, d)
+            else:
+                self.rest = 'r1'
+        else:
+            if (dots):
+                dots = int(dots) * "."
+            self.rest = 'r' + self.parserDuration[duration] + dots
+
+
+    def getRest(self):
+        return self.rest
+
+
+class TypeNote(TypeDuration):
+    parserTpc = {
+        '-1' : 'feses',
+        '0' : 'ceses',
+        '1' : 'geses',
+        '2' : 'deses',
+        '3' : 'ases',
+        '4' : 'eses',
+        '5' : 'bes',
+        '6' : 'fes',
+        '7' : 'ces',
+        '8' : 'ges',
+        '9' : 'des',
+        '10' : 'as',
+        '11' : 'es',
+        '12' : 'b',
+        '13' : 'f',
+        '14' : 'c',
+        '15' : 'g',
+        '16' : 'd',
+        '17' : 'a',
+        '18' : 'e',
+        '19' : 'h',
+        '20' : 'fis',
+        '21' : 'cis',
+        '22' : 'gis',
+        '23' : 'dis',
+        '24' : 'ais',
+        '25' : 'eis',
+        '26' : 'his',
+        '27' : 'fisis',
+        '28' : 'cisis',
+        '29' : 'gisis',
+        '30' : 'disis',
+        '31' : 'aisis',
+        '32' : 'eisis',
+        '33' : 'hisis'
+    }
+
+    def __init__(self, lastPitch, pitch, lastTpc, tpc, duration, dots):
+        self.line = ""
+        self.line += self.parserTpc[tpc]
+        self.pitch = pitch
+        pitchDiff = int(pitch) - int(lastPitch)
+        tpcDiff = int(tpc) - int(lastTpc)
+        print("%%%% pitchDiff %s, lastPitch %s, pitch %s, tpcDiff %s" % (pitchDiff, lastPitch, pitch, tpcDiff))
+        #TODO: clean up this mess
+        if (pitchDiff >= 6 and pitchDiff < 18):
+            if (pitchDiff == 6 and tpcDiff == 6):
+                print("%% pitchDiff > but exception")
+                self.line += ""
+            else:
+                print("%% pitchDiff >")
+                self.line += "'"
+        elif (pitchDiff >= 18 and pitchDiff < 30):
+            if (pitchDiff == 18 and tpcDiff == 6):
+                print("%% pitchDiff >> but exception")
+                self.line += "'"
+            else:
+                print("%% pitchDiff >>")
+                self.line += "''"
+        elif (pitchDiff >= 30):
+            if (pitchDiff == 30 and tpcDiff == 6):
+                print("%% pitchDiff >>> but exception")
+                self.line += "''"
+            else:
+                print("%% pitchDiff >>>")
+                self.line += "'''"
+        elif (pitchDiff <= -6 and pitchDiff > -18):
+            if (pitchDiff == -6 and tpcDiff == -6):
+                print("%% pitchDiff < but exception")
+                self.line += ""
+            else:
+                print("%% pitchDiff <")
+                self.line += ","
+        elif (pitchDiff <= -18 and pitchDiff > -30):
+            if (pitchDiff == -18 and tpcDiff == -6):
+                print("%% pitchDiff << but exception")
+                self.line += ","
+            else:
+                print("%% pitchDiff <<")
+                self.line += ",,"
+        elif (pitchDiff <= -30):
+            if (pitchDiff == -30 and tpcDiff == -6):
+                print("%% pitchDiff <<< but exception")
+                self.line += ",,"
+            else:
+                print("%% pitchDiff <<<")
+                self.line += ",,,"
+        self.line += self.parserDuration[duration]
+        if (dots):
+            self.line += int(dots) * "."
+    
+    def getNote(self):
+        return self.line
+
+    def getPitch(self):
+        return self.pitch
+
+
+class TypeBarLine():
+    parser = {
+        "startRepeat" : ".|:",
+        "endRepeat" : ":|.",
+        "double" : "||",
+        "end" : "|."
+    }
+
+    def __init__(self, line):
+        self.line = self.parser[line]
+
+    def getLine(self):
+        return self.line
+
+
+class TypeBarValidator():
+    def __init__(self):
+        pass
+
+
+class TypeTie():
+    def __init__(self):
+        pass
+
+    def getTie(self):
+        return "~"
+
+
+class TypeSlur():
+    def __init__(self, type):
+        self.type = type
+
+    def getSlur(self):
+        if self.type == "start":
+            return "("
+        else:
+            return ")"
+
+
+class TypeRehearsalMark():
+    def __init__(self, text):
+        self.text = text    
+
+    def getMark(self):
+        return "\\mark \\markup { \\box \\bold %s }" % self.text
+
+
+class TypeVolta():
+    def __init__(self, type, voltaText, voltaEndType):
+        self.type = type
+        self.voltaText = voltaText
+        self.voltaEndType = voltaEndType
+
+    def getEnd(self):
+        return self.voltaEndType
+
+    def getVolta(self):
+        if (self.type == "start"):
+            return "\\set Score.repeatCommands = #\'((volta \"%s\"))" % self.voltaText
+        elif (self.type == "end"):
+            return "\\set Score.repeatCommands = #\'((volta #f))"
+
+
+class TypeLayoutBreak():
+    def __init__(self, text): 
+        self.text = text
+
+    def getBreak(self):
+        return "\\break"
+
+
+class TypeLyrics():
+    def __init__(self, no, text, syllabic):
+        self.no = no
+        self.text = text
+        self.syllabic = syllabic
+
+    def getNo(self):
+        return self.no
+
+    def getText(self):
+        return self.text
+
+    def hasSyllabic(self):
+        return self.syllabic in ["begin", "middle"]
+
+    def getSyllabic(self):
+        return "--"
+
+
+class TypeClef():
+    types = {
+        "G8vb" : "tenorG",
+        "F" : "bass",
+        '' : "treble"
+    }
+    def __init__(self, type): 
+        self.type = type
+
+    def getType(self):
+        return "\\clef %s" % self.types[self.type]
+
 
 def get_musescore(filename):
     parser = XmlParser(context=XmlContext())
@@ -17,15 +293,7 @@ def get_first_element(element):
         return element
 
 
-@dataclass
-class Header:
-    title: str
-    subtitle: str
-    composer: str
-    lyricist: str
-
-
-def get_header(musescore: MuseScore) -> Header:
+def get_header(musescore: MuseScore) -> TypeHeader:
     title = ""
     subtitle = ""
     composer = ""
@@ -40,7 +308,7 @@ def get_header(musescore: MuseScore) -> Header:
                 composer = t.text
             case "Lyricist":
                 lyricist = t.text
-    return Header(title=title, subtitle=subtitle, composer=composer, lyricist=lyricist)
+    return TypeHeader(title=title, subtitle=subtitle, composer=composer, lyricist=lyricist)
 
 
 def get_measure_count(musescore: MuseScore) -> int:
@@ -70,42 +338,26 @@ def get_measure(musescore: MuseScore, index: int, staff: int = 0) -> Optional[Me
 def get_time_signature(measure: Measure) -> Optional[str]:
     for t in measure.voice.content:
         if isinstance(t, TimeSig):
-            return f"{t.sig_n}/{t.sig_d}"
+            return TypeTimeSignature(t.sig_n, t.sig_d)
     else:
         return None
-
-
-class TypeKeySignature():
-    parser = {
-        -7: 'ces',
-        -6: 'ges',
-        -5: 'des',
-        -4: 'as',
-        -3: 'es',
-        -2: 'b',
-        -1: 'f',
-        0: 'c',
-        1: 'g',
-        2: 'd',
-        3: 'a',
-        4: 'e',
-        5: 'h',
-        6: 'fis',
-        7: 'cis',
-    }
 
 
 def get_key_signature(measure: Measure, is_first_measure=False) -> Optional[str]:
     for k in measure.voice.content:
         if isinstance(k, KeySig):
-            return TypeKeySignature.parser[k.accidental]
+            return TypeKeySignature(k.accidental)
     if is_first_measure:
-        return TypeKeySignature.parser[0]
+        return TypeKeySignature(0)
     return None
 
 
 def parse_measure(measure: Measure) -> Optional[str]:
-    return measure
+    content = []
+    for e in measure.voice.content:
+        if isinstance(e, Rest):
+            content.append(TypeRest(e.duration_type, None, ""))
+    return content
 
 
 if __name__ == "__main__":
--- scripts/test_parser.py
+++ scripts/test_parser.py
@@ -1,22 +1,41 @@
 import unittest
 from parser import *
+import functools
 
+def xprint(*args, **kwargs):
+    print(*args, **kwargs)
+
+indent = 0
+def monitor_results(func):
+    @functools.wraps(func)
+    def wrapper(*func_args, **func_kwargs):
+        global indent
+        xprint(f"{'  ' * indent}=> {func.__name__}({func_args}{func_kwargs})")
+        indent += 1
+        retval = func(*func_args, **func_kwargs)
+        indent -= 1
+        xprint(f"{'  ' * indent}<= {func.__name__}() ret {repr(retval)}")
+        return retval
+    return wrapper
 
 class TestMethods(unittest.TestCase):
 
+    @monitor_results
     def test_header(self):
         musescore = get_musescore("msmodel_examples/test_header.mscx")
-        get_header(musescore)
+        xprint(get_header(musescore))
 
+    @monitor_results
     def test_time_signature(self):
         musescore = get_musescore("msmodel_examples/test_time_signature.mscx")
         for i in range(get_measure_count(musescore)):
-            get_time_signature(get_measure(musescore, i))
+            xprint(get_time_signature(get_measure(musescore, i)))
 
+    @monitor_results
     def test_key_signature(self):
         musescore = get_musescore("msmodel_examples/test_key_signature.mscx")
         for i in range(get_measure_count(musescore)):
-            get_key_signature(get_measure(musescore, i), is_first_measure=(False, True)[i == 0])
+            xprint(get_key_signature(get_measure(musescore, i), is_first_measure=(False, True)[i == 0]))
 
 
 if __name__ == '__main__':
